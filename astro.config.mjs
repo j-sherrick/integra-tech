d402fe2120943eb0fd60892846476dd1e4aab470
Jason Sherrick <jasonsherrick@gmail.com>
2023-08-27 20:33:06 -0500

    added netlify configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,4 +16,6 @@ export default defineConfig({
     mdx(),
     sitemap(),
   ],
+  output: 'server',
+  adapter: netlify(),
 });
